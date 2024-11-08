f4b069a21d038285dc8f3d516b26a4cb678b17e8
Alexis-Valles <L24550808@chihuahua2.tecnm.mx>
2024-11-08 17:03:32 -0600

    Add files via upload

---
+++ EXA2_EVALUACION_24550808/src/main/java/com/mycompany/exa2_evaluacion_24550808/EXA2_EVALUACION_24550808.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exa2_evaluacion_24550808;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alexi
+ */
+public class EXA2_EVALUACION_24550808 {
+
+    public static void main(String[] args) {
+        int rounds = 10;
+        int computerWins = 0, userWins = 0, empate;
+        String[]computer1{papel,piedra,tijera,papel,piedra,tijera,papel,piedra,tijera};
+         String[]user {papel,piedra,tijera,papel,piedra,tijera,papel,piedra,tijera}; 
+         
+         for(int i = 0; i < rounds; i++){
+    }
+        
+        
+        
+       
+    }
+}
